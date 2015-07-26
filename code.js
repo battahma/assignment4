c3a118561d9b19c89d859adf4abb615d56066397
Tyler Koistinen <koistinentyler@gmail.com>
2015-07-26 12:36:28 -0700

    Favorite buttons working

--- code.js
+++ code.js
@@ -5,9 +5,10 @@ var url1 = "https://api.github.com/gists?page=1&per_page=75";
 var url2 = "https://api.github.com/gists?page=2&per_page=75";
 var gistObjArray = [];
 var gistsToDisplay = [];
-var gistToDisplayFavorits = [];
+var gistsToDisplayFavorites = [];
 makeAjaxCall(url1);
 makeAjaxCall(url2);
+var currentPage = 1;
 
 var FavoriteList = document.getElementById("favList");
 
@@ -16,10 +17,10 @@ for(var i=0; i<navBtns.length; i++){
     navBtns[i].addEventListener('click', navClick, false);
 }
 
-function findbyUrl(url){
+function findbyUrl(url, callback){
 	for (var i =0; i<gistsToDisplay.length; i++){
-		if(gistsToDisplay[i]==url){
-			return i;
+		if(gistsToDisplay[i].url==url){
+			callback( i);
 		}
 	}
 }
@@ -31,7 +32,35 @@ function gist(requestObj){
 	this.convertToHTML = function(whichList){
         if(whichList === "Favoites")
 		{
-			
+			var d = document.createElement('div');
+            var a = document.createElement("a");
+            var btn = document.createElement("Button");
+            btn.innerHTML = "+";
+            btn.className = "btn btn-primary";
+            var myURL = this.url;
+            btn.onclick = function (){
+                var index = 0;
+                for (var i =0; i<gistsToDisplay.length; i++){
+                    if(gistsToDisplayFavorites[i].url==myURL){
+                        index = i;
+                    }
+                }
+                gistToDisplay.push(gistsToDisplayFavorites[index]);
+                gistsToDisplayFavorites.splice(index,1);
+                viewPage(currentPage);
+                viewPageFavorites(currentPage);
+            }
+            
+            //var a = document.createAttribute("href");
+            a.setAttribute("href", this.url);
+            var text = document.createTextNode(this.description);
+            if(this.description == null || this.description.length == 0){
+                text = document.createTextNode("No Description");
+            }
+            a.appendChild(text);
+            d.appendChild(btn);
+            d.appendChild(a);
+            return d;
 		}
 		else{
 			var d = document.createElement('div');
@@ -39,10 +68,18 @@ function gist(requestObj){
 			var btn = document.createElement("Button");
 			btn.innerHTML = "+";
 			btn.className = "btn btn-primary";
+            var myURL = this.url;
 			btn.onclick = function (){
-				//To do: get id of the the div
-				//Remove it from Regular search
-				//Add it to the favourites list 
+                var index = 0;
+                for (var i =0; i<gistsToDisplay.length; i++){
+                    if(gistsToDisplay[i].url==myURL){
+                        index = i;
+                    }
+                }
+				gistsToDisplayFavorites.push(gistsToDisplay[index]);
+                gistsToDisplay.splice(index,1);
+                viewPage(currentPage);
+                viewPageFavorites(currentPage);
 			}
 			
 			//var a = document.createAttribute("href");
@@ -71,6 +108,7 @@ function makeAjaxCall(url){
                 serverResponse.forEach(function(i){
                     var g = new gist(i);
                     gistObjArray.push(g);
+                    searchBtn.onclick();
                 })
             }
             else{
@@ -81,6 +119,7 @@ function makeAjaxCall(url){
 }
 
 function viewPage(pageNum){
+    currentPage = pageNum;
     pageNum--;
     while(results.firstChild){
         results.removeChild(results.firstChild);
@@ -107,6 +146,36 @@ function viewPage(pageNum){
         results.appendChild(li);
     }
 }
+
+function viewPageFavorites(pageNum){
+    currentPage = pageNum;
+    pageNum--;
+    while(FavoriteList.firstChild){
+        FavoriteList.removeChild(FavoriteList.firstChild);
+    }
+    if(gistsToDisplayFavorites.length != 0){
+        if(pageNum*30 <= gistsToDisplayFavorites.length || (pageNum == 0 && gistsToDisplayFavorites.length>0)){
+            for(var i = pageNum*30; (i < pageNum*30+30 && i<gistsToDisplayFavorites.length); i++){
+                var li =  document.createElement("li");
+                li.appendChild(gistsToDisplayFavorites[i].convertToHTML("General"));
+                FavoriteList.appendChild(li);
+            }
+        }
+        else{
+            var li =  document.createElement("li");
+            text = document.createTextNode("No results on this page");
+            li.appendChild(text);
+            FavoriteList.appendChild(li);
+        }
+    }
+    else{
+        var li =  document.createElement("li");
+        text = document.createTextNode("No results");
+        li.appendChild(text);
+        FavoriteList.appendChild(li);
+    }
+}
+
 
 function navClick(){
     var whichBtn = this.getAttribute("id");
